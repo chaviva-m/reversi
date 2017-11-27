a39a3b92862b662735dbbd0288d9e3b53e8781c6
Chaviva Moshavi <chaviva.moshavi@gmail.com>
2017-11-27 20:07:26 +0200

    added tests for getPossibleMoves

--- test/test_StandardGameLogic.cpp
+++ test/test_StandardGameLogic.cpp
@@ -12,6 +12,9 @@
 
 //test getPossibleMoves in standard game logic when there are no possible moves
 TEST_F(StandardGameLogicTest, NoPossibleMoves) {
+  //board is empty
+  Board empty_board(r_, c_);
+  EXPECT_TRUE(logic_.getPossibleMoves(empty_board, BLACK).empty());
   //empty spaces but no where to go
   EXPECT_TRUE(logic_.getPossibleMoves(board_, BLACK).empty());
   //full board
@@ -24,13 +27,24 @@ TEST_F(StandardGameLogicTest, NoPossibleMoves) {
 
 //test getPossibleMoves in standard game logic when there are possible moves
 TEST_F(StandardGameLogicTest, PossibleMoves) {
-  vector<Cell*> moves;
-  moves.reserve(c_);
-  for (int j = 0; j < c_; j++) {
-    moves.push_back(board_.getCell(r_-1, j));
+  //one possible move
+  Board almost_full_board = board_;
+  for (int j = 0; j < c_ - 1; j++) {
+    almost_full_board.getCell(r_-1, j)->insertDisk(BLACK);
   }
-  ASSERT_EQ(logic_.getPossibleMoves(board_, WHITE).size(), moves.size());
+  vector<Cell*> one_move;
+  one_move.push_back(almost_full_board.getCell(r_-1, c_-1));
+  ASSERT_EQ(logic_.getPossibleMoves(almost_full_board, WHITE).size(),
+                                                    one_move.size());
+  EXPECT_EQ(*logic_.getPossibleMoves(almost_full_board, WHITE)[0], *one_move[0]);
+  //multiple possible moves
+  vector<Cell*> mult_moves;
+  mult_moves.reserve(c_);
+  for (int j = 0; j < c_; j++) {
+    mult_moves.push_back(board_.getCell(r_-1, j));
+  }
+  ASSERT_EQ(logic_.getPossibleMoves(board_, WHITE).size(), mult_moves.size());
   for (unsigned i = 0; i < logic_.getPossibleMoves(board_, WHITE).size(); i++) {
-    EXPECT_EQ(logic_.getPossibleMoves(board_, WHITE)[i], moves[i]);
+    EXPECT_EQ(*logic_.getPossibleMoves(board_, WHITE)[i], *mult_moves[i]);
   }
 }
